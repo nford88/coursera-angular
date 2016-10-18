8baf57abc36f7fc576bb8d823b9fd52260a9b876
nford88 <nford.work@gmail.com>
2016-10-18 14:55:02 +0100

    Update

---
+++ mod1_sol/app.js
@@ -0,0 +1,51 @@
+// (function () {
+// 'use strict';
+//
+// angular.module('LunchCheck', [])
+// .controller('LunchCheckController', LunchCheckController);
+//
+// LunchCheckController.$inject = ['$scope'];
+//   function LunchCheckController($scope){
+//     $scope.box = "John";
+//     $scope.noMeals function (box, seperator){
+//         var box = $scope.box;
+//         console.log(box);
+//
+//     };
+//
+// }
+// })();
+
+(function() {
+
+  'use strict';
+
+  angular.module('LunchCheck', []).
+
+  controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+
+  function LunchCheckController($scope) {
+    $scope.lunch = ''; //user's inpuy that holds the luch
+    $scope.message = ''; //output message
+
+    $scope.checkIfTooMuch = function() {
+      //check if input is empty
+      if($scope.lunch.length === 0) {
+        $scope.message = 'Please enter data first!';
+        $scope.style = 'text-danger';
+        $scope.borderStyle = 'redborder';
+      }else {
+        //use split method to split the input lunch in the various items
+        var dishes = $scope.lunch.split(',');
+        //check the number of items to return the apppropriate message
+        $scope.message = dishes.length > 3 ? 'Too much!' : 'Enjoy!';
+        $scope.style = 'text-success';
+        $scope.borderStyle = 'greenborder';
+      }
+    };
+
+  };
+
+})();
