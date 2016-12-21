8be147a40708410e88cfc42883f4ac4dc7e61568
nford88 <nford.work@gmail.com>
2016-12-21 10:40:43 +0000

    Mod 2

---
+++ mod2_sol/app.js
@@ -0,0 +1,35 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService)
+
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var buy = this;
+
+  buy.items = ShoppingListCheckOffService.getItems();
+
+  buy.itemName = "";
+  buy.itemQuantity = "";
+
+  buy.addItem = function () {
+      ShoppingListCheckOffService.addItem(buy.itemName, buy.itemQuantity);
+    };
+
+  buy.removeItem = function (itemIndex) {
+    ShoppingListCheckOffService.removeItem(itemIndex);
+  };
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var bought = this;
+
+
+}
+
+});
